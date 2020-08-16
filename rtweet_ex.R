bbde96b88882e8d484f70dff99a5298cdd2df53d
IsabellaGomide020 <i175293@dac.unicamp.br>
2020-08-16 00:02:35 -0300

    coloquei aspas em tudo

--- rtweet_ex.R
+++ rtweet_ex.R
@@ -35,14 +35,41 @@ sp <- search_tweets("isabella", geocode = lookup_coords("brazil",apikey = google
 #Ex: Procurar filtrando Latitude,Longitude,Raio
 
 #Palavras de interesse
-dicionario <- c('corona',  'covid', 'quarentena', 'lockdown', 'cloroquina', 'casos', 'ministros' ,'colapso','respiradores','isolamento horizontal','isolamento vertical','quarentena','hospital','campanha','isolamento social','distanciamento social,'máscara','epidemia','pandemia','contágio','confinamento','assintomático','OMS','saúde pública','pico','UTI','comorbidade','obesidade','diabete','doenças respiratórias','doenças do coração','cardíacos','diabéticos','pneumonia','vacina','casa','idosos','crianças','medidas','economia','Manaus','São Paulo','Rio de Janeiro','febre','dor de cabeça','saudade','luto','mortes','óbitos','aglomeração','casos graves','suspeita','casos confirmados','testes','subnotificação','solidariedade','doações','cestas básicas','risco','epicentro','comércio','home office','hospitais de campanha', 'auxílio emergencial','fique em casa','desemprego','empresários','Brasil','China','Itália','Estados Unidos','Espanha','França','suspensão do contrato de trabalho','leitos','países','infecção','contágio','contaminados','países','casos notificados','casos confirmados','casos descartados','pacientes curados','ciência','médicos','medicina','pesquisadores','salário','distanciamento','governadores','dinheiro público',sus','corrupção','responsabilidade','achatamento da curva','estatísticas','mortalidade','letalidade','transporte público','rodízio','lotação','enfermeiros','hidroxicloroquina','gripezinha','clima','cardiovascular','crise sanitária','cloroquina','álcool','lavar as mãos','medidas', 'caos social', 'fome', 'violência doméstica','cidades','favelas','microempresas','microempresário','funcionários','funcionários público','Suécia','depressão','escolas','shoppings','afastamento') 
+dicionario <- c("corona", "covid", "quarentena", "lockdown", "cloroquina", "casos", "ministros" ,"colapso",
+                "respiradores","isolamento horizontal","isolamento vertical","quarentena","hospital",
+                "campanha","isolamento social","distanciamento social","máscara", "epidemia",
+                "pandemia","contágio","confinamento","assintomático","OMS","saúde pública",
+                "pico","UTI","comorbidade","obesidade","diabete","doenças respiratórias",
+                "doenças do coração","cardíacos","diabéticos","pneumonia","vacina",
+                "casa","idosos",'"crianças","medidas","economia","Manaus","São Paulo",
+                "Rio de Janeiro","febre","dor de cabeça","saudade","luto","mortes","óbitos",
+                "aglomeração',"casos graves","suspeita","casos confirmados","testes",
+                "subnotificação","solidariedade","doações","cestas básicas","risco","epicentro",
+                "comércio","home office","hospitais de campanha","auxílio emergencial","fique em casa",
+                "desemprego","empresários","Brasil","China","Itália","Estados Unidos","Espanha",
+                "França","suspensão do contrato de trabalho","leitos","países","infecção",
+                "contágio","contaminados","países","casos notificados","casos confirmados",
+                "casos descartados","pacientes curados","ciência","médicos","medicina",
+                "pesquisadores","salário","distanciamento","governadores","dinheiro público",
+                "sus","corrupção","responsabilidade","achatamento da curva","estatísticas",
+                "mortalidade","letalidade","transporte público","rodízio","lotação","enfermeiros",
+                "hidroxicloroquina","gripezinha","clima","cardiovascular","crise sanitária",
+                "cloroquina","álcool","lavar as mãos","medidas","caos social","fome","violência doméstica",
+                "cidades","favelas","microempresas","microempresário","funcionários","funcionários público",
+                "Suécia","depressão","escolas","shoppings","afastamento") 
 
 
 
 tweets_brazil <- search_tweets(dicionario, 
                            geocode = "-10,-55,600mi", n = 100)
 
-contas <- c(colocar os @@@@@@)
+contas <- c("jairbolsonaro","jdoriajr","wilsonwitzel","RomeuZema","EduardoLeite_","costa_rui","
+            CarlosMoises","CamiloSantanaCE","PauloCamara40","ratinho_jr","ronaldocaiado","helderbarbalho",
+            "FlavioDino","wdiaspi","joaoazevedolins","MauroMendes40","belivaldochagas","Casagrande_ES",
+            "RenanFilho_","GovernoAlagoas","gladsoncameli","maurocarlesse","GovernoRO","celmarcosrocha",
+            "Reinaldo45psdb","waldezoficial","antoniodenarium","AbrahamWeint","DamaresAlves","rsallesmma"
+            "PauloGuedesMin","TerezaCrisMS","SF_Moro","tarcisiogdf","OsmarTerra","indaiatubapref",
+            "prefpauliniasp","prefsp","sumaresp","pref_sorocaba","onasdonizette_")
 
 #Palavras de interesse
 
